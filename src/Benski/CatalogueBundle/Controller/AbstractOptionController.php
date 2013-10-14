a372b0a92743d33c8495c778837b8fffa58afbf7
SulivanDotEu <benjamin.ellis@sulivan.eu>
2013-10-14 10:10:42 +0200

    commit benskiv2 14 10 2013

--- src/Benski/CatalogueBundle/Controller/AbstractOptionController.php
+++ src/Benski/CatalogueBundle/Controller/AbstractOptionController.php
@@ -5,7 +5,7 @@ namespace Benski\CatalogueBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Benski\CatalogueBundle\Entity\AbstractOption;
+use Benski\CatalogueBundle\Entity\Option\AbstractOption;
 
 /**
  * AbstractOption controller.
@@ -22,7 +22,7 @@ class AbstractOptionController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('BenskiCatalogueBundle:AbstractOption')->findAll();
+        $entities = $em->getRepository('BenskiCatalogueBundle:Option\AbstractOption')->findAll();
 
         return $this->render('BenskiCatalogueBundle:AbstractOption:index.html.twig', array(
             'entities' => $entities,
@@ -37,7 +37,7 @@ class AbstractOptionController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('BenskiCatalogueBundle:AbstractOption')->find($id);
+        $entity = $em->getRepository('BenskiCatalogueBundle:Option\AbstractOption')->find($id);
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find AbstractOption entity.');
